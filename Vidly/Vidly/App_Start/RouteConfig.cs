229cda2b9414938437e8a2c1a17ca95d5b51769c
CoreyCooley <Corey.a.Cooley@gmail.com>
2017-07-07 21:57:25 -0400

    Udemy ASP.Net MVC 5 Section 2 Complete.

--- Vidly/Vidly/App_Start/RouteConfig.cs
+++ Vidly/Vidly/App_Start/RouteConfig.cs
@@ -13,6 +13,21 @@ namespace Vidly
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
+
+            /*
+            // Order of Routes matter list from specific to general
+            // This is not the best way to do custom routes!! With MVC 5 we can use a attribute routing
+            routes.MapRoute(
+                name: "MoviesByReleaseDate",
+                url: "movies/released/{year}/{month}",
+                defaults: new { controller = "Movies", action = "ByReleaseDate"},
+                constraints: new { year = @"\d{4}", month = @"\d{2}"}
+                //constraints: new { year = @"2015|2016", month = @"\d{2}" } // This allows you to add contraints on the year
+            );
+            */
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Corey"},
+                new Customer {Name = "Brittany"}
+            };
+
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            // You can pass data to the views Don't do the two below
+            // ViewData["Movie"] = movie;
+            // No Compile Time Check
+            //ViewBag.Movie = movie;
+            // return View();
+
+            // Helper Method Inherited from the Base Class Controller
+            // return View(movie);
+            return View(viewModel);
+
+            // Other ActionResults:
+            // return Content("Hello World!");
+            // return HttpNotFound();
+            // return new EmptyResult();
+            // return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name"});
+        }
+
+        // Get: Movies/Edit/id
+        public ActionResult Edit(int id)
+        {
+            return Content("Id = " + id);
+
+        }
+
+        // Get: Movies/Index/
+        // int? == the int par is optional
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if(!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageIndex=={0} and sortBy={1}", pageIndex, sortBy));
+        }
+
+        // Use Attribute Routes for Custome Routing
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
+        // Get: Movies/Release/Year/Month
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+
+            return Content(year + "/" + month);
+        }
+
+    }
+}
