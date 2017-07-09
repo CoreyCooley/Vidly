2abae3fd8d9a42c7362834193da9cc22a8dfb1a1
CoreyCooley <Corey.a.Cooley@gmail.com>
2017-07-08 23:24:24 -0400

    Solution - Section 3 Exercise - Display Membership Type

--- Vidly/Vidly/Controllers/CustomersController.cs
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -5,15 +5,32 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //  Must Dispose of the ApplicationDbContext
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            //var customers = GetCustomers();
+            // This gets the customers from the database - This is called deffered execution
+            var customers = _context.Customers.Include( c => c.MembershipType).ToList();
 
             return View(customers);
         }
@@ -21,7 +38,8 @@ namespace Vidly.Controllers
         // GET: Customers/Details/id
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            //var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
