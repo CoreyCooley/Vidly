c34bd023e33fd638c57e330a88cb723a090fbca3
CoreyCooley <Corey.a.Cooley@gmail.com>
2017-07-13 23:05:20 -0400

    Solution - Section 5 Exercise - Add Validation to Movie Form

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -12,7 +12,6 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-
         private ApplicationDbContext _context;
 
         public MoviesController()
@@ -20,25 +19,23 @@ namespace Vidly.Controllers
             _context = new ApplicationDbContext();
         }
 
-        //  Must Dispose of the ApplicationDbContext
         protected override void Dispose(bool disposing)
         {
             _context.Dispose();
         }
 
-        // GET: Movies
         public ViewResult Index()
         {
-            //var movies = GetMovies();
             var movies = _context.Movies.Include(m => m.Genre).ToList();
 
             return View(movies);
         }
 
-        public ActionResult New()
+        public ViewResult New()
         {
             var genres = _context.Genres.ToList();
-            var viewModel = new MovieFormViewModel()
+
+            var viewModel = new MovieFormViewModel
             {
                 Genres = genres
             };
@@ -46,67 +43,33 @@ namespace Vidly.Controllers
             return View("MovieForm", viewModel);
         }
 
-        [HttpPost]
-        public ActionResult Save(Movie movie)
-        {
-            // If the Movie Id is 0 it is a new movie
-            if (movie.Id == 0)
-            {
-                movie.DateAdded = DateTime.Now;
-                _context.Movies.Add(movie);
-            }
-            else
-            {
-                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
-
-                movieInDb.Name = movie.Name;
-                movieInDb.ReleaseDate = movie.ReleaseDate;
-                movieInDb.GenreId = movie.GenreId;
-                movieInDb.NumberInStock = movie.NumberInStock;
-
-            }
-
-            _context.SaveChanges();
-           
-            return RedirectToAction("Index", "Movies");
-        }
-
         public ActionResult Edit(int id)
         {
-            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
 
             if (movie == null)
                 return HttpNotFound();
 
-            var viewModel = new MovieFormViewModel
+            var viewModel = new MovieFormViewModel(movie)
             {
-                Movie = movie,
                 Genres = _context.Genres.ToList()
             };
 
             return View("MovieForm", viewModel);
         }
 
-        // GET: Movies/Details/id
+
         public ActionResult Details(int id)
         {
-            //var movie = GetMovies().SingleOrDefault(c => c.Id == id);
-            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(c => c.Id == id);
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
 
             if (movie == null)
                 return HttpNotFound();
 
             return View(movie);
+
         }
 
-        private IEnumerable<Movie> GetMovies()
-        {
-            return new List<Movie>
-            {
-                new Movie { Id = 1, Name = "Shrek" },
-                new Movie { Id = 2, Name = "Wall-e" }
-            };
-        }
 
         // GET: Movies/Random
         public ActionResult Random()
@@ -127,14 +90,37 @@ namespace Vidly.Controllers
             return View(viewModel);
         }
 
-        // Use Attribute Routes for Custome Routing
-        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
-        // Get: Movies/Release/Year/Month
-        public ActionResult ByReleaseDate(int year, int month)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Save(Movie movie)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new MovieFormViewModel(movie)
+                {
+                    Genres = _context.Genres.ToList()
+                };
 
-            return Content(year + "/" + month);
+                return View("MovieForm", viewModel);
+            }
+
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
         }
-
     }
 }
